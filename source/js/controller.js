f5beabbd070ebe1b3fecef2a8c42bed3501b618d
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-25 15:13:40 -0200

    add the angular initial files and structure

---
+++ source/js/controller.js
@@ -0,0 +1,25 @@
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'formApp' )
+    .controller( 'FormController', FormController );
+
+  FormController.$inject = [];
+
+  function FormController() {
+
+    var vm = this;
+
+    ///////////// Properties
+
+    ///////////// Public Methods
+
+    ///////////// Private Methods
+
+    /////////////
+
+  }
+
+}());
